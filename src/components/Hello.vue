84b8285f567ca3e8f46de994b28b9ef0b9007712
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-11-23 18:06:21 -0800

    Use Sass to preprocess Vue component styles

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -85,18 +85,15 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="sass" scoped>
+h1, h2
+  font-weight: normal
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+ul
+  list-style-type: none
+  padding: 0
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+li
+  display: inline-block
+  margin: 0 10px
 </style>
