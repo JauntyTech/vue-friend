1109b4f9b00ec407e3dbf8bb087a2fe55ecda958
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-11-23 17:14:09 -0800

    Use Element in project
    Use English language pack and theme from generated theme/ directory.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,23 +1,76 @@
 <template>
-  <div class="hello">
+  <el-card class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li>
+        <a href="https://vuejs.org" target="_blank">
+          <el-button>
+            Core Docs
+          </el-button>
+        </a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">
+          <el-button>
+            Forum
+          </el-button>
+        </a>
+      </li>
+      <li>
+        <a href="https://gitter.im/vuejs/vue" target="_blank">
+          <el-button>
+            Gitter Chat
+          </el-button>
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">
+          <el-button>
+            Twitter
+          </el-button>
+        </a>
+      </li>
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
+          <el-button>
+            Docs for This Template
+          </el-button>
+        </a>
+      </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">
+          <el-tag>
+            vue-router
+          </el-tag>
+        </a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">
+          <el-tag>
+            vuex
+          </el-tag>
+        </a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">
+          <el-tag>
+            vue-loader
+          </el-tag>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
+          <el-tag>
+            awesome-vue
+          </el-tag>
+        </a>
+      </li>
     </ul>
-  </div>
+  </el-card>
 </template>
 
 <script>
@@ -45,9 +98,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
