fcf14ea4a67fbcfaa0f4118cb3388115916d12a8
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-11-25 01:07:34 -0800

    Add Vuex module for state-diagram-related state management

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import components from './modules/components'
 import projects from './modules/projects'
+import stateDiagram from './modules/stateDiagram'
 
 Vue.use(Vuex)
 
@@ -10,7 +11,8 @@ const DEBUG = process.env.NODE_ENV === 'development'
 export default new Vuex.Store({
   modules: {
     components,
-    projects
+    projects,
+    stateDiagram
   },
   strict: DEBUG
 })
---
+++ src/store/modules/stateDiagram.js
@@ -0,0 +1,41 @@
+import mockStateDiagram from '../../api/mockStateDiagram.json'
+import * as getterNames from '../getterNames'
+
+const state = {
+  stateDiagram: mockStateDiagram
+}
+
+const getters = {
+  [getterNames.stateDiagramActions]: (state, getters) => {
+    if (!getters.selectedProjectId) return null
+    return state.stateDiagram[getters.selectedProjectId].actions
+  },
+
+  [getterNames.stateDiagramComponents]: (state, getters) => {
+    if (!getters.selectedProjectId) return null
+    return state.stateDiagram[getters.selectedProjectId].components
+  },
+
+  [getterNames.stateDiagramMutations]: (state, getters) => {
+    if (!getters.selectedProjectId) return null
+    return state.stateDiagram[getters.selectedProjectId].mutations
+  },
+
+  [getterNames.stateDiagramStateVariables]: (state, getters) => {
+    if (!getters.selectedProjectId) return null
+    return state.stateDiagram[getters.selectedProjectId].stateVariables
+  }
+}
+
+const actions = {
+}
+
+const mutations = {
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
